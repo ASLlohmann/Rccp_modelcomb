f43e80ffd3d71b5b6ca6ce4efe649d9601fdd624
ASLlohmann <61584526+ASLlohmann@users.noreply.github.com>
2020-10-18 13:47:42 +0100

    Update help.cpp

--- help.cpp
+++ help.cpp
@@ -3,9 +3,12 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-bool ishybrid(List mod) {
-is = mod.inherits("hybridModel");
-  return is;
-  }
+void checkCVArguments_rcpp(NumericVector x,int windowSize, int maxHorizon){
+if (x.size() < windowSize + 2 * maxHorizon) stop("The time series must be longer than windowSize + 2 * maxHorizon.");
+if (windowSize <= 0 || maxHorizon <= 0) stop("The arguments windowSize, and maxHorizon must be positive integers.");
+if (int(windowSize) == false ||  int(maxHorizon) == false) stop("The arguments windowSize, and maxHorizon must be positive integers.");
+}
+
+
 
 
