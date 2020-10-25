e3a6679a4672e3eab9ccc5b498619b7d5a21f1e8
ASLlohmann <61584526+ASLlohmann@users.noreply.github.com>
2020-10-25 16:37:44 +0000

    Update transcription.cpp

--- transcription.cpp
+++ transcription.cpp
@@ -1,6 +1,7 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+//checkCVArguemnts function
 
 // [[Rcpp::export]]
 void checkCVArguments_rcpp(NumericVector x,int windowSize, int maxHorizon){
@@ -9,6 +10,7 @@ if (windowSize <= 0 || maxHorizon <= 0) stop("The arguments windowSize, and maxH
 if (int(windowSize) == false ||  int(maxHorizon) == false) stop("The arguments windowSize, and maxHorizon must be positive integers.");
 }
 
+//tsPartition function
 
 // [[Rcpp::export]]
 cppFunction('Rcpp::Range tsPartition(NumericVector y, int windowSize, int maxHorizon){
@@ -18,6 +20,8 @@ int start = 1;
 Rcpp::Range s=Rcpp::seq(start, start + windowSize -1);
 return s; 
  }')
+
+//Core CVTS function
  
 // [[Rcpp::export]]             
 cppFunction('NumericMatrix m3(int numRow, int MaxHorizon){
@@ -25,4 +29,11 @@ NumericVector v = rep({NA_REAL},numRow*MaxHorizon);
 NumericMatrix m3(numRow , MaxHorizon, v.begin() );
 return m3;
 }')
+            
+// [[Rcpp::export]]                     
+cppFunction('List vector(int numRow){
+List L = rep(List::create(NA_INTEGER),numRow);
+return L;
+}')
+
   
