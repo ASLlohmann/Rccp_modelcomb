351f5dcf52c9b936588103b90934e263da97a774
ASLlohmann <61584526+ASLlohmann@users.noreply.github.com>
2020-11-04 18:18:51 +0000

    Update TsPartition_rcpp.cpp

--- TsPartition_rcpp.cpp
+++ TsPartition_rcpp.cpp
@@ -1,6 +1,19 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+
+
+// [[Rcpp::export]]
+cppFunction('NumericVector tsSubsetWithIndices(NumericVector tsTrain, NumericVector trainIndices) {
+Function timer("time");  
+NumericVector time=timer(tsTrain);
+double minv = min(trainIndices);
+double maxv = max(trainIndices);
+Function windowr("window");  
+NumericVector wdw=windowr(tsTrain, Named("start")=time[minv], _["end"]=time[maxv]);
+return wdw;
+}')
+
 // [[Rcpp::export]]
 List  tsPartition(double rolling, NumericVector x,NumericVector windowSize,NumericVector maxHorizon){
 
